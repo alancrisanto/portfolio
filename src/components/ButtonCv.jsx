be9ed02096931162f1ec9c188d0180bc967360ff
acrisanto <alanvcrisanto@gmail.com>
2024-10-24 01:53:12 -0500

    update cv

--- src/components/ButtonCv.jsx
+++ src/components/ButtonCv.jsx
@@ -3,7 +3,7 @@ function ButtonCv() {
 		<div>
 			<a
 				className=" bg-green-600 hover:bg-green-400 py-1 px-3 rounded-md text-slate-100 inline-block"
-				href="/AlanCV_2024.pdf"
+				href="/AcrisCv.pdf"
 				download
 			>
 				Download Cv
